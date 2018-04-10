2913cf182ebccb988d356c02fed58558fbcc9e2a
Nataša Andžić <natasa.andzic@yahoo.com>
2018-04-10 21:06:33 +0200

    UI style (colors etc) fixed

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -16,6 +16,7 @@
 package com.example.android.miwok;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,9 +29,12 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word>  {
 
+    private int mColorResourceId;
 
-    public WordAdapter(Context context, ArrayList<Word> words) {
+
+    public WordAdapter(Context context, ArrayList<Word> words, int colorResourceId) {
         super(context, 0, words);
+        mColorResourceId=colorResourceId;
     }
 
     @Override
@@ -57,6 +61,10 @@ public class WordAdapter extends ArrayAdapter<Word>  {
 
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        View textContainer = listItemView.findViewById(R.id.textContainer);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textContainer.setBackgroundColor(color);
 
         return listItemView;
     }

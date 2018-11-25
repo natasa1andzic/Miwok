8e970e64cd9eb6a2f244a973b0e0d0bd35794e43
Natasa <natasa.andzic@yahoo.com>
2018-11-25 22:18:28 +0100

    AudioManager class and listeners added

--- app/src/main/java/com/example/android/activities/FamilyActivity.java
+++ app/src/main/java/com/example/android/activities/FamilyActivity.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.activities;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,13 +32,52 @@ import adapters.WordAdapter;
 
 public class FamilyActivity extends AppCompatActivity {
 
-    MediaPlayer mp;
+	/* Handles playback of all the sound files */
+	private MediaPlayer mp;
+
+	/* Handles audio focus when playing a sound file */
+	private AudioManager audioManager;
+
+	/**
+	 * This listener gets triggered whenever the audio focus changes
+	 * (i.e., we gain or lose audio focus because of another app or device).
+	 */
+	private AudioManager.OnAudioFocusChangeListener onAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+		@Override
+		public void onAudioFocusChange(int focusChange) {
+			if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+					focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+				// The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a short amount of time.
+				// The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that our app is allowed to continue playing sound but at a lower volume.
+				// We'll treat both cases the same way because our app is playing short sound files.
+
+				// Pause playback and reset player to the start of the file. That way, we can play the word from the beginning when we resume playback.
+				mp.pause();
+				mp.seekTo(0);
+			} else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+				// The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+				mp.start();
+			} else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+				// The AUDIOFOCUS_LOSS case means we've lost audio focus and stop playback and clean up resources
+				releaseMediaPlayer();
+			}
+		}
+	};
+
+	/* This listener gets triggered when the {@link MediaPlayer} has completed playing the audio file. */
+	private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+		@Override
+		public void onCompletion(MediaPlayer mediaPlayer) {
+			// Now that the sound file has finished playing, release the media player resources.
+			releaseMediaPlayer();
+		}
+	};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
+	    audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         // Create a list of words
        final ArrayList<Word> words = new ArrayList<Word>();
@@ -68,11 +109,39 @@ public class FamilyActivity extends AppCompatActivity {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Word word = words.get(position);
-                mp = MediaPlayer.create(FamilyActivity.this, word.getmAudioResourceId());
-                mp.start();
-                Toast.makeText(FamilyActivity.this, "List item clicked", Toast.LENGTH_SHORT).show();
+	            releaseMediaPlayer();
+            	Word word = words.get(position);
+
+	            // Request audio focus so in order to play the audio file. The app needs to play a
+	            // short audio file, so we will request audio focus with a short amount of time with AUDIOFOCUS_GAIN_TRANSIENT.
+	            int result = audioManager.requestAudioFocus(onAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+	            if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
+		            // We have audio focus now.
+		            // Create and setup the {@link MediaPlayer} for the audio resource associated with the current word
+		            mp = MediaPlayer.create(FamilyActivity.this, word.getmAudioResourceId());
+	            mp.start();
+	            mp.setOnCompletionListener(mCompletionListener);
             }
         });
     }
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mp != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mp.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mp = null;
+        }
+    }
+	protected void onStop(){
+		super.onStop();
+		releaseMediaPlayer();
+	}
 }
--- app/src/main/java/com/example/android/activities/NumbersActivity.java
+++ app/src/main/java/com/example/android/activities/NumbersActivity.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.activities;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -30,49 +32,114 @@ import adapters.WordAdapter;
 
 public class NumbersActivity extends AppCompatActivity {
 
-    MediaPlayer mp;
+	/* Handles playback of all the sound files */
+	private MediaPlayer mp;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-       // getActionBar().setDisplayHomeAsUpEnabled(true);
+	/* Handles audio focus when playing a sound file */
+	private AudioManager audioManager;
 
-        // Create a list of words
-       final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word(R.drawable.number_one,"one", "lutti",R.raw.number_one));
-        words.add(new Word(R.drawable.number_two,"two", "otiiko", R.raw.number_two));
-        words.add(new Word(R.drawable.number_three,"three", "tolookosu", R.raw.number_three));
-        words.add(new Word(R.drawable.number_four,"four", "oyyisa",R.raw.number_four));
-        words.add(new Word(R.drawable.number_five,"five", "massokka", R.raw.number_five));
-        words.add(new Word(R.drawable.number_six,"six", "temmokka", R.raw.number_six));
-        words.add(new Word(R.drawable.number_seven,"seven", "kenekaku",R.raw.number_seven));
-        words.add(new Word(R.drawable.number_eight,"eight", "kawinta",R.raw.number_eight));
-        words.add(new Word(R.drawable.number_nine,"nine", "wo’e",R.raw.number_nine));
-        words.add(new Word(R.drawable.number_ten,"ten", "na’aacha", R.raw.number_ten));
+	/**
+	 * This listener gets triggered whenever the audio focus changes
+	 * (i.e., we gain or lose audio focus because of another app or device).
+	 */
+	private AudioManager.OnAudioFocusChangeListener onAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+		@Override
+		public void onAudioFocusChange(int focusChange) {
+			if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+					focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+				// The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a short amount of time.
+				// The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that our app is allowed to continue playing sound but at a lower volume.
+				// We'll treat both cases the same way because our app is playing short sound files.
 
-        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-        // adapter knows how to create list items for each item in the list.
-        WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
+				// Pause playback and reset player to the start of the file. That way, we can play the word from the beginning when we resume playback.
+				mp.pause();
+				mp.seekTo(0);
+			} else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+				// The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+				mp.start();
+			} else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+				// The AUDIOFOCUS_LOSS case means we've lost audio focus and stop playback and clean up resources
+				releaseMediaPlayer();
+			}
+		}
+	};
 
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml layout file.
-        ListView listView = (ListView) findViewById(R.id.list);
+	/* This listener gets triggered when the {@link MediaPlayer} has completed playing the audio file. */
+	private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+		@Override
+		public void onCompletion(MediaPlayer mediaPlayer) {
+			// Now that the sound file has finished playing, release the media player resources.
+			releaseMediaPlayer();
+		}
+	};
 
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
-        listView.setAdapter(adapter);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.word_list);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Word word = words.get(position);
-                mp = MediaPlayer.create(NumbersActivity.this, word.getmAudioResourceId());
-                mp.start();
-                Toast.makeText(NumbersActivity.this, "List item clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+		// Create a list of words
+		final ArrayList<Word> words = new ArrayList<Word>();
+		words.add(new Word(R.drawable.number_one, "one", "lutti", R.raw.number_one));
+		words.add(new Word(R.drawable.number_two, "two", "otiiko", R.raw.number_two));
+		words.add(new Word(R.drawable.number_three, "three", "tolookosu", R.raw.number_three));
+		words.add(new Word(R.drawable.number_four, "four", "oyyisa", R.raw.number_four));
+		words.add(new Word(R.drawable.number_five, "five", "massokka", R.raw.number_five));
+		words.add(new Word(R.drawable.number_six, "six", "temmokka", R.raw.number_six));
+		words.add(new Word(R.drawable.number_seven, "seven", "kenekaku", R.raw.number_seven));
+		words.add(new Word(R.drawable.number_eight, "eight", "kawinta", R.raw.number_eight));
+		words.add(new Word(R.drawable.number_nine, "nine", "wo’e", R.raw.number_nine));
+		words.add(new Word(R.drawable.number_ten, "ten", "na’aacha", R.raw.number_ten));
+
+		// Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+		// adapter knows how to create list items for each item in the list.
+		WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
+
+		// Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+		// There should be a {@link ListView} with the view ID called list, which is declared in the
+		// word_list.xml layout file.
+		ListView listView = (ListView) findViewById(R.id.list);
+
+		// Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+		// {@link ListView} will display list items for each {@link Word} in the list.
+		listView.setAdapter(adapter);
+
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				releaseMediaPlayer();
+				Word word = words.get(position);
+
+				// Request audio focus so in order to play the audio file. The app needs to play a
+				// short audio file, so we will request audio focus with a short amount of time with AUDIOFOCUS_GAIN_TRANSIENT.
+				int result = audioManager.requestAudioFocus(onAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+				if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
+					// We have audio focus now.
+					// Create and setup the {@link MediaPlayer} for the audio resource associated with the current word
+					mp = MediaPlayer.create(NumbersActivity.this, word.getmAudioResourceId());
+				mp.start();
+				mp.setOnCompletionListener(mCompletionListener);
+			}
+		});
+	}
+
+	/*Clean up the media player by releasing its resources.*/
+	private void releaseMediaPlayer() {
+		// If the media player is not null, then it may be currently playing a sound.
+		if (mp != null) {
+			// Regardless of the current state of the media player, release its resources because we no longer need it.
+			mp.release();
+			// Set the media player back to null. For our code, we've decided that setting the media player to null
+			// is an easy way to tell that the media player is not configured to play an audio file at the moment.
+			mp = null;
+		}
+	}
+
+	protected void onStop() {
+		super.onStop();
+		releaseMediaPlayer();
+	}
 }
